5999504301ce81737cda3742d4ea767ce49e9035
Megagig <megagigsoftwaresolutions.com>
2024-11-11 16:23:15 +0100

    Changed the PORT in the env variable

--- Backend/index.js
+++ Backend/index.js
@@ -2,9 +2,13 @@ import express from 'express';
 import { connectDB } from './db/connectDB.js';
 import authRoute from './routes/auth.route.js';
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -12,7 +16,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/auth', authRoute);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port`, PORT);
 });
