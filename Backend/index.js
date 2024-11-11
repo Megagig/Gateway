a544028457cf8bb80733bb84c403df0eace9dc4d
Megagig <megagigsoftwaresolutions.com>
2024-11-11 15:45:13 +0100

    Creating Authentication Routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import { connectDB } from './db/connectDB.js';
+import authRoute from './routes/auth.route.js';
 const app = express();
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.use('/api/v1/auth', authRoute);
 
 app.listen(3000, () => {
   connectDB();
