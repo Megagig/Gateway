f4e8ad97ab6de58c1f3fc8e3e53e6f3a6620a7c1
Megagig <megagigsoftwaresolutions.com>
2024-11-11 13:15:14 +0100

    Create connectDB.js in a Database Folder:

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import { connectDB } from './db/connectDB.js';
 const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(3000, () => {
+  connectDB();
   console.log('Server is running on port 3000');
 });
