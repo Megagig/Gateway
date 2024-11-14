88c6b9cd4f9dc08adac83bdc3adc1f42412f9c75
Megagig <megagigsoftwaresolutions.com>
2024-11-14 11:07:30 +0100

    Implemented Cors to the backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { connectDB } from './db/connectDB.js';
 import authRoute from './routes/auth.route.js';
 const app = express();
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.use(express.json()); // allow us to parse incoming json data
 
